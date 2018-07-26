c563d27c0931deff2d9d1d71517d7fd0052a0609
macalimosa <calimosa_mark@yahoo.com.ph>
2018-07-26 18:37:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="pay.php" method="post">
+    <label for="">Card No.</label>
+    <input type="text" name="card_number" value="4111111111111111">
+    <br>
+    <label for="">Expiration Date</label>
+    <input type="text" name="card_exp" value="2038-12">
+    <br>
+    <label for="">Amount</label>
+    <input type="text" name="amount" value="151.51">
+    <br>
+    <input type="submit" value="Submit">
+
+  </form>
+</body>
+</html>
